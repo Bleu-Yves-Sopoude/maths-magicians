dba479e56cd1ca20e8699406f302f4259f63f070
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-07-20 16:40:54 +0000

    Delete files

---
+++ src/components/Result.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './result.css'
+
+
+function Result(){
+
+    return(
+        <div className="resultContainer">
+            <h1 className="result">0</h1> 
+        </div>
+
+    );
+
+}
+
+export default Result;
