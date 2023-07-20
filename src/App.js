e46326bf4f0d655b911aa5669e6b41a35f94f81b
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-07-20 15:01:17 +0000

    Fix linters

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello, Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <p>
+         Hello, Edit <code>src/App.js</code>
+         and save to reload.
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
