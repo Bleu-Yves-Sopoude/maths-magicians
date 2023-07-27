c171c89a1210e21279172dccde41ada21e13fc30
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-07-27 09:29:15 +0000

    Fix linter

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,51 +1,56 @@
 // src/App.js
-import React from 'react';
- // Make sure to use the correct path to Calculator.j
-import { useState, useEffect } from "react"
+import React from "react";
+// Make sure to use the correct path to Calculator.j
+import { useState, useEffect } from "react";
 
 function Quotes() {
-  const [data, setData] = useState([])
-  const [hasError, setHasError] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const [data, setData] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(true)
+      setIsLoading(true);
       try {
-        const res = await fetch("https://api.api-ninjas.com/v1/quotes?category=success",{method: 'GET',
-        headers: {
-            'X-Api-Key': 'r2CBEN9bBC4tHiqsYKa6/g==Ymb9Ax7CT5bvAly0',
-        }},)
-        
-        const json = await res.json()
-        setData(json)
-        console.log(json)
+        const res = await fetch(
+          "https://api.api-ninjas.com/v1/quotes?category=success",
+          {
+            method: "GET",
+            headers: {
+              "X-Api-Key": "r2CBEN9bBC4tHiqsYKa6/g==Ymb9Ax7CT5bvAly0",
+            },
+          }
+        );
+
+        const json = await res.json();
+        setData(json);
       } catch (error) {
-        setHasError(true)
+        setHasError(true);
       }
-      setIsLoading(false)
-    }
-    fetchData()
-  }, [setData, setIsLoading])
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [setData, setIsLoading]);
 
-  if (hasError) return <div>Something went wrong!</div>
+  if (hasError) return <div>Something went wrong!</div>;
 
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div>
-
-<h2 class="ml-2 font-bold text-white text-align: center; dark:text-gray-400 bg-green-500 p-6 rounded-lg shadow-lg justify-center  ">
-           
-    <ul>
-      {data.map((item) => (
-        <li key={item[0]}>{item.quote} -- {item.author}</li>
-        
-
-      ))}
-    </ul>
-    </h2>
-  </div>)
+      <h2 class="ml-2 font-bold text-white text-align: center; dark:text-gray-400 bg-green-500 p-6 rounded-lg shadow-lg justify-center  ">
+        <ul>
+          {data.map((item) => (
+            <li key={item[0]}>
+              {item.quote}
+              --
+              {item.author}
+            </li>
+          ))}
+        </ul>
+      </h2>
+    </div>
+  );
 }
 
 export default Quotes;
