6affbcb9819a406dff1da952bf7f16ea5a1482c8
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-08-03 08:52:25 +0000

    Fix linter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 // src/App.js
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
-import Calculator from "./components/Calculator"; // Make sure to use the correct path to Calculator.js
-import Quotes from "./components/Quotes";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Calculator from './components/Calculator'; // Make sure to use the correct path to Calculator.js
+import Quotes from './components/Quotes';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 const App = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path="/home" element={<Home />}>
-        {" "}
+      <Route path='/home' element={<Home />}>
+        {' '}
       </Route>
-      <Route path="/calculator" element={<Calculator />}>
-        {" "}
+      <Route path='/calculator' element={<Calculator />}>
+        {' '}
       </Route>
-      <Route path="/quote" element={<Quotes />}>
-        {" "}
+      <Route path='/quote' element={<Quotes />}>
+        {' '}
       </Route>
     </Routes>
   </BrowserRouter>
